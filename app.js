af32f23823957e2e020c512829eeeaea776b734d
colw <colw@outlook.com>
2016-12-29 16:02:34 +0100

    Refactored redis
    - Store item under hash
    - Tag references
    - Tag counts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -8,7 +9,7 @@ app.set('port', (process.env.PORT || 9000));
 var storage = new (require('./storage'))();
 var nxws = new (require('./fetchfeed.js'));
 var types = require('./types');
-var feedDict = require('./feeds.json').remote;
+var feedDict = require('./feeds.json').local;
 var feeds = Object.keys(feedDict).map(function(x) {return feedDict[x]});
 
 function storeItem(err, item) {
@@ -31,7 +32,32 @@ function sendAllArticles(socket) {
   });
 }
 
-nxws.fetchSourceFromStream(feeds, request, storeItem);
+function sendTopWords(socket) {
+  // storage.getTopWords(10).then(x => console.log('top words', x))
+  storage.getTopWords(10).then(list => {
+    socket.emit('nxws top10', JSON.stringify(list.map(x => {
+      return {word: x};
+    })));
+  });
+}
+
+var initialisedNewsDB = false;
+
+function broadcastArticle(article) {
+  if (initialisedNewsDB) {
+    console.log('new article received', article);
+    io.emit('nxws items', JSON.stringify(newItem));
+  }
+}
+
+storage.setListener(broadcastArticle.bind(this));
+
+function fetchNews() {
+  nxws.fetchSourceFromStream(feeds, fs.createReadStream, storeItem);
+  // storage.getTopWords(10).then(x => console.log('top words', x))
+}
+
+setInterval(fetchNews, 10000);
 
 /* Server */
 http.listen(app.get('port'), function() {
@@ -44,6 +70,7 @@ io.on('connection', function(socket) {
   console.log("User connected", socket.id);
 
   sendAllArticles(socket); // TODO pay attention to route
+  sendTopWords(socket);
 
   socket.on('disconnect', function() {
   	console.log("User disconnected", socket.id);
--- storage.js
+++ storage.js
@@ -1,21 +1,60 @@
 "use strict";
 
+const util = require('util');
 var redis = require("redis"),
-	shortid = require('shortid');
-    
+	crypto = require('crypto'),
+	stopwords = require('./dict');
+	// stopwords = require('./natural/lib/natural/util/stopwords').words;
+
+var natural = require('natural');
+natural.PorterStemmer.attach();
+let tokenizer = new natural.WordTokenizer();
+
+// var WordPOS = require('wordpos');
+// wordpos = new WordPOS({stopwords: true});
+
+var WordPOS = require('wordpos'),
+    wordpos = new WordPOS({stopwords: true});
+// wordpos.isAdjective('fast', console.log);
+
+// console.log("i am waking up to the sounds of chainsaws".tokenizeAndStem());
+// console.log("chainsaws".stem());
+
+// natural.LancasterStemmer.attach();
+// console.log("i am waking up to the sounds of chainsaws".tokenizeAndStem());
+// console.log("chainsaws".stem());
+
+// process.exit();
+
+let storage_constants = {
+	ALL: "*",
+	COUNT: "*count"
+}
+
 module.exports = class Storage {
+
 	constructor() {
 		this.store = redis.createClient()
-		this.store.on('connect', () => {console.log('Connected')})
+		this.store.on('connect', () => { console.log('Connected') })
 		this.store.on("error", function (err) {
-		    console.log(err)
+			console.log(err)
 		});
+	}
+
+	setListener(fn) {
+		this.newItemListener = fn ? fn : () => {};
+	}
+
+	broadcastArticle(article) {
+		this.newItemListener(article);
 	}
 
 	DBResolve(DBMethod, item, ...params) {
 		return new Promise((resolve, reject) => {
 			DBMethod(...params, (err, result) => {
+				// console.log('result', result);
 				if (err) {
+					console.log('err', result, item);
 					return reject(err);
 				}
 				return resolve(item);
@@ -23,63 +62,154 @@ module.exports = class Storage {
 		});
 	}
 
-	addItemToDBSortedSet(item, key) {
+	_dbAddItemToSortedSet(item, hash) {
 		const score = (new Date(item.date)).getTime();
-		const member = item.itemID;
-
-		return this.DBResolve(this.store.zadd.bind(this.store),	item,
-							  key, score, member);
-
+		const value = item.itemID;
+		return this.DBResolve(this.store.zadd.bind(this.store), item,
+			hash, score, value);
 	}
 
-	addItemToDBHash(item) {
-		const hash = item.itemID;
+	inStopWords(word) {
+		// console.log(stopwords);
+		return stopwords.indexOf(word) >= 0;
+	}
 
+	zipNewsItem(item) {
 		let fieldValueZip = [];
 		for (const key of Object.keys(item)) {
 			fieldValueZip.push(key)
-			fieldValueZip.push(item[key] || "n/a"); //TODO Hmm. Update this.
-		}
 
+			if (key === 'tags') {
+				// console.log('ARRAY>>>>>>>>\n', item.tags, '\n\n');
+				fieldValueZip.push(JSON.stringify(item.tags)); //TODO Hmm. Update this.
+			} else {
+				fieldValueZip.push(item[key] || "n/a"); //TODO Hmm. Update this.
+			}
+
+		}
+		return fieldValueZip;
+	}
+
+	_dbAddItemToHash(item) {
+		const hash = item.itemID;
+		fieldValueZip = this.zipNewsItem(item);
 		return this.DBResolve(this.store.hmset.bind(this.store), item,
-							  hash, ...fieldValueZip);
+			hash, ...fieldValueZip);
 	}
 
 	breakStringIntoTags(sentence) {
 		var words = [];
 		var re = /([\w+]+)/g;
 		let result = '';
-		while ((result = re.exec(sentence)) !== null) {
-			words.push(result[0])
-		}
-		// console.log(words);
-		return words;
+
+		return new Promise((resolve, reject) => {
+			wordpos.getNouns(sentence, function(result) {
+				// console.log(result);
+				return resolve(result);
+			});
+		});
 	}
 
 	breakIntoTags(item) {
-		let ps = [];
-		for (const word of this.breakStringIntoTags(item.title)) {
-			ps.push(this.addItemToDBSortedSet(item, word.toLowerCase()));
-		}
-		return ps;
+		return this.breakStringIntoTags(item.title).then((tokens) => {
+			let ps = [];
+			for (const word of tokens) {
+				let w = word.toLowerCase();
+				var pr = this._dbAddItemToSortedSet(item, w)
+					.then((z) => {
+						let w2 = word.toLowerCase();
+						// console.log('w', w2);
+						let c = this.DBResolve(this.store.zincrby.bind(this.store), item, storage_constants.COUNT, 1, w2)
+						// console.log('adding', z, ' -- ', pr, w2, item.title, c);
+						return c;
+					});
+				ps.push(pr);
+			}
+			return ps;
+		});
 	}
 
+	createTagsFromItem(item) {
+		const sw = this.inStopWords;
+		return new Promise((resolve, reject) => {
+			wordpos.getNouns(item.title, function(result) {
+				const newResult = result.map(x => x.toLowerCase()).filter(x => !sw(x));
+				// console.log('removed n words', result.length - newResult.length);
+				return resolve(newResult);
+			});
+		});
+	}
+
+	// this.DBResolve(this.store.hmset.bind(this.store), item, hash, ...fieldValueZip);
+	// this.DBResolve(this.store.zadd.bind(this.store), item, key, score, value);
+	// this.DBResolve(this.store.zincrby.bind(this.store), item, constants.COUNT, 1, tag)
+
 	add(item) {
-		item.itemID = shortid.generate();
+		item.itemID = crypto.createHash('md5').update(item.guid).digest("hex");
 
-		const masterTag = this.addItemToDBSortedSet(item, '_');
-		const otherTags = this.breakIntoTags(item);
+		return this.store.exists(item.itemID, (err, reply) => {
+			if (reply === 1) {
+				// console.log('exists');
+				return;
+			} else {
 
-		return Promise.all([masterTag, ...otherTags])
-			.then(values => {
-				this.addItemToDBHash(values[0]);
-			})
-			.catch((err) => {console.log('ERROR:', err)})
+				return this.createTagsFromItem(item)
+					// Attach tags to item
+					.then(tags => {
+						item.tags = tags;//.map(x => x.toLowerCase());
+						return item;
+					})
+					// Add News Item to DB
+					.then(item => {
+						return this.DBResolve(this.store.hmset.bind(this.store), item, item.itemID, ...this.zipNewsItem(item));
+					})
+					// Add tags to Database
+					.then(item => {
+						var tagPromises = [];
+						const score = (new Date(item.date)).getTime();
+						for (let i = 0; i < item.tags.length; i++) {
+							let tag = item.tags[i];//.toLowerCase();
+							let tp = this.DBResolve(this.store.zadd.bind(this.store), item, tag, score, item.itemID);
+							tagPromises.push(tp)
+						}
+						let tpALL = this.DBResolve(this.store.zadd.bind(this.store), item, storage_constants.ALL, score, item.itemID);
+						tagPromises.push(tpALL);
+						return Promise.all(tagPromises).then(() => item)
+					})
+					// Increase counts of tags
+					.then(item => {
+						let tagPromises = [];
+						for (let i = 0; i < item.tags.length; i++) {
+							let tag = item.tags[i];//.toLowerCase();
+							let tp = this.DBResolve(this.store.zincrby.bind(this.store), item, storage_constants.COUNT, 1, tag);
+							tagPromises.push(tp)
+						}
+						return Promise.all(tagPromises).then(() => item)
+					})
+					.then(item => {
+						// console.log('item done', item);
+						return item;
+					})
+					.then(this.broadcastArticle.bind(this))
+					.catch((err) => { console.log('ERROR:', err) });
+			}
+		});
+	}
+
+	getTopWords(n) {
+		return new Promise((resolve, reject) => {
+			this.store.zrevrange(storage_constants.COUNT, 0, n - 1, (err, result) => {
+				if (err) {
+					return reject(err);
+				} else {
+					return resolve(result);
+				}
+			});
+		});
 	}
 
 	getSortedSet(id) {
 		return new Promise((resolve, reject) => {
-			console.log
 			this.store.zrange(id, 0, -1, (err, result) => {
 				if (err) {
 					return reject(err);
@@ -89,10 +219,9 @@ module.exports = class Storage {
 			});
 		});
 	}
-	
+
 	getHash(id) {
 		return new Promise((resolve, reject) => {
-			console.log
 			this.store.hgetall(id, (err, result) => {
 				if (err) {
 					return reject(err);
@@ -104,10 +233,9 @@ module.exports = class Storage {
 	}
 
 	getAll() {
-		return this.getSortedSet('_')
+		return this.getSortedSet(storage_constants.ALL)
 			.then(idList => {
 				let newsListPromises = [];
-				// console.log('idList', idList);
 				for (const id of idList) {
 					newsListPromises.push(this.getHash(id));
 				}
